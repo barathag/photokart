110a127290ac3ed2d5d9311d591bcce29be412b1
barathag <barath.1902016@srit.org>
2024-01-12 10:37:42 +0530

    first_commit

---
+++ src/main/java/com/photomagic/photokart/PhotokartApplication.java
@@ -0,0 +1,13 @@
+package com.photomagic.photokart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotokartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotokartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photomagic/photokart/PhotokartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photomagic.photokart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotokartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
